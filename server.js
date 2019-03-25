de4510083198e25dff9fd2cc6ba9c865f94df217
VladMogwai <vlad.arefiev@gmail.com>
2019-03-25 20:22:55 +0200

    new try

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const SocketServer = require('ws').Server;
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+const INDEX = path.join(__dirname, '.client/index.html');
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .use((req, res) => res.sendFile(INDEX))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new SocketServer({ server });
 
