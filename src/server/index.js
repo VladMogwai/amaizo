4803e6c97844933d7b1b121cd8c6ab8a385e7a04
VladMogwai <vlad.arefiev@gmail.com>
2019-03-27 17:08:47 +0200

    changed port

--- src/server/index.js
+++ src/server/index.js
@@ -10,6 +10,6 @@ app.get('/api/getUsername', (req, res) =>
   res.send({ username: os.userInfo().username })
 );
 
-app.listen(process.env.PORT || 8080, () =>
-  console.log(`Listening on port ${process.env.PORT || 8080}!`)
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Listening on port ${process.env.PORT || 5000}!`)
 );
