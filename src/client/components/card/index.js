aa62ddc2c5f46d8956c0de67be8a0922e1aaf7cc
VladMogwai <vlad.arefiev@gmail.com>
2019-04-17 19:24:32 +0300

    nosferatu

--- src/client/components/card/index.js
+++ src/client/components/card/index.js
@@ -9,6 +9,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import vampireImg from '../../static/images/vampire.jpg';
+import nosferatuImg from '../../static/images/nosferatu.jpg';
 
 const styles = {
   card: {
@@ -23,6 +24,36 @@ const styles = {
 function ImgMediaCard(props) {
   const { classes } = props;
   return (
+    <div>
+      <Card className={classes.card}>
+      <CardActionArea>
+      <CardMedia
+          component="img"
+          alt="nosferatu"
+          className={classes.media}
+          height="140"
+          image={nosferatuImg}
+          title="nosferatu"
+        />
+      <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Sabbat
+          </Typography>
+          <Typography component="p">
+            The most visibly cursed of all Kindred, the Embrace warps each Nosferatu into a hideously deformed creature; 
+            the archetypal Nosferatu resembles Max Shreck's Count Orlok, though the curse has any number of variations. 
+            The Nosferatu are the spymasters of the dead, collecting information and selling it for a dear price. 
+            They are also the masters of the underground, living in the sewers for protection.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+      
+    </div>
     <Card className={classes.card}>
       <CardActionArea>
         <CardMedia
@@ -32,7 +63,7 @@ function ImgMediaCard(props) {
           height="140"
           image={vampireImg}
           title="vampire"
-        />
+        />        
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             Sabbat
