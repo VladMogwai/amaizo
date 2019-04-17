3bdbcff9b49c0cd59f723456d0d5d43be9ee66f1
VladMogwai <vlad.arefiev@gmail.com>
2019-04-17 19:27:55 +0300

    fix build

--- src/client/components/card/index.js
+++ src/client/components/card/index.js
@@ -26,62 +26,65 @@ function ImgMediaCard(props) {
   return (
     <div>
       <Card className={classes.card}>
-      <CardActionArea>
-      <CardMedia
-          component="img"
-          alt="nosferatu"
-          className={classes.media}
-          height="140"
-          image={nosferatuImg}
-          title="nosferatu"
-        />
-      <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Sabbat
-          </Typography>
-          <Typography component="p">
-            The most visibly cursed of all Kindred, the Embrace warps each Nosferatu into a hideously deformed creature; 
-            the archetypal Nosferatu resembles Max Shreck's Count Orlok, though the curse has any number of variations. 
-            The Nosferatu are the spymasters of the dead, collecting information and selling it for a dear price. 
-            They are also the masters of the underground, living in the sewers for protection.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-      
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="nosferatu"
+            className={classes.media}
+            height="140"
+            image={nosferatuImg}
+            title="nosferatu"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Sabbat
+            </Typography>
+            <Typography component="p">
+              The most visibly cursed of all Kindred, the Embrace warps each
+              Nosferatu into a hideously deformed creature; the archetypal
+              Nosferatu resembles Max Shreck's Count Orlok, though the curse has
+              any number of variations. The Nosferatu are the spymasters of the
+              dead, collecting information and selling it for a dear price. They
+              are also the masters of the underground, living in the sewers for
+              protection.
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="vampire"
+            className={classes.media}
+            height="140"
+            image={vampireImg}
+            title="vampire"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Sabbat
+            </Typography>
+            <Typography component="p">
+              The Sabbat is a loose organization of Cainites who reject the
+              Traditions. Unlike the Camarilla, the Sabbat believes in the
+              Antediluvians and Caine. Also known as the Sword of Caine, as they
+              believe they will be the army Caine will use to destroy the
+              Antediluvians once Gehenna arrives.
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
     </div>
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="vampire"
-          className={classes.media}
-          height="140"
-          image={vampireImg}
-          title="vampire"
-        />        
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Sabbat
-          </Typography>
-          <Typography component="p">
-            The Sabbat is a loose organization of Cainites who reject the
-            Traditions. Unlike the Camarilla, the Sabbat believes in the
-            Antediluvians and Caine. Also known as the Sword of Caine, as they
-            believe they will be the army Caine will use to destroy the
-            Antediluvians once Gehenna arrives.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
   );
 }
 
